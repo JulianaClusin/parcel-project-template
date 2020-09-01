0d77d825bc69bb6342974772466b0cd3f32ea5b6
JulianaClusin <naymenko95@gmail.com>
2020-09-02 02:47:07 +0300

    Updates

--- src.f2ee298f.js
+++ src.f2ee298f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-danylko-1/src.65920d1c.js.map
+//# sourceMappingURL=/project-danylko-1/src.f2ee298f.js.map
